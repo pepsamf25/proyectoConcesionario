0e7a8ad596d03b291d803b1ef246eaa06ca4104c
Francesco <francesdbz18pc@outlook.es>
2026-05-18 11:46:32 +0200

    añadir funciones auxiliares, requisitos y más tareas a TODO.md

--- TODO.md
+++ TODO.md
@@ -3,3 +3,35 @@
 ### Jenkins:
 
 - Solucionar envío de informes.
+
+### Seguridad:
+- Inyección XSS:
+    - Controlar el Content-type de cada petición 
+    - Utilizar el método before_request en el archivo app.py
+    - En todas las rutas se utiliza g.cleaned_json. 
+    - HAY QUE VALIDAR (comparar tipos, tamaño, posibles valores,,) TODOS LOS DATOS QUE SE RECIBEN Y SOBRE TODO SI SON ARCHIVOS
+    - Sanitizar/Codificar la salida: en todos los controladores donde se devuelvan datos. Por ejemplo, en el archivo controladores_chuches.py
+- Cifrado de claves:
+    - Utilizar funciones en controlador_usuarios.py: login_usuario y alta_usuario
+- Evitar CSRF:
+    - Añadir protección a app.py
+    - Añadir protección a index.html
+    - Añadir token CSRF a las páginas que lo requieran
+- Establecer cabeceras seguras:
+    - Cambiar app.py
+    - Cambiar cómo se da la respuesta en todas las rutas
+- Gestionar la sesión segura:
+    - Con cookies:
+        - Modificar app.py
+        - Modificar controlador_usuarios.py
+        - En controlador_usuarios.py establecer la ruta de cerrar sesión
+        - En todas las rutas comprobar si el usuario tiene permiso o no
+    - Con auth
+    - Con tokens
+- Generar logs:
+    - Añadir líneas a app.py
+    - En cualquier sitio que se quiera grabar en el log escribir app.logger.info("texto").
+- Comprobar que subir archivos y ver archivos este securizado
+- Conexión a la base de datos segura
+- Configurar python para utilizar https
+
--- api/web/funciones_auxiliares.py
+++ api/web/funciones_auxiliares.py
@@ -1,8 +1,75 @@
 import decimal
 import json
+import bleach
+import html
+from flask import session
 
 class Encoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, decimal.Decimal): return float(obj)
 
+def sanitize_field(data):
+    if isinstance(data, str):
+        return bleach.clean(html.escape(data))
+    if isinstance(data, dict):
+        return {k: sanitize_field(v) for k, v in data.items()}
+    if isinstance(data, list):
+        return [sanitize_field(v) for v in data]
+    return data
 
+def prepare_response_extra_headers(include_security_headers):
+
+    response_extra_headers = {
+        # always
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+        'Last-Modified': http_date(datetime.datetime.now()),
+        'Server':''
+    }
+    if include_security_headers:
+        response_security_headers = {
+            # X-Frame-Options: page can only be shown in an iframe of the same site
+            'X-Frame-Options': 'SAMEORIGIN',
+            # ensure all app communication is sent over HTTPS
+            'Strict-Transport-Security': 'max-age=63072000; includeSubdomains',
+            # instructs the browser not to override the response content type
+            'X-Content-Type-Options': 'nosniff',
+            # enable browser cross-site scripting (XSS) filter
+            'X-XSS-Protection': '1; mode=block'
+        }
+        response_extra_headers.update(response_security_headers)
+
+    return response_extra_headers
+
+def create_session(usuario,perfil):
+    session["usuario"] = usuario
+    session["perfil"] = perfil
+
+
+def delete_session():
+    session.clear()
+
+
+def validar_session_normal():
+    try:
+        if session["usuario"] and session["usuario"] != "":
+            return True
+        else:
+            return False
+    except Exception:
+        return False
+
+
+def validar_session_admin():
+    try:
+        if (
+            session["usuario"]
+            and session["usuario"] != ""
+            and session["perfil"] == "admin"
+        ):
+            return True
+        else:
+            return False
+    except Exception:
+        return False
